8acf4cddce849dabeb95f1e63bbc4d3171b12fb2
Kiran200113 <kiran.gadani@hitechsmartsolutions.com>
2024-03-08 18:13:28 +0530

    checking

--- src/app/api/services/Constants.ts
+++ src/app/api/services/Constants.ts
@@ -1,6 +1,8 @@
-// export const API_ENDPOINT = 'https://localhost:44310/api/';
-export const API_ENDPOINT = 'https://dashboard.revords.com/testapi/api/';
-export const DownloadAPK_ENDPOINT = 'https://dashboard.revords.com/testapi/wwwroot/'
+// export const API_ENDPOINT = 'https://dashboard.revords.com/testapi/api/';
+// export const DownloadAPK_ENDPOINT = 'https://dashboard.revords.com/testapi/wwwroot/'
+
+export const API_ENDPOINT = 'http://ho.hitechprojects.co.in:8101/api/';
+export const DownloadAPK_ENDPOINT = 'http://ho.hitechprojects.co.in:8101/wwwroot/'
 
 export const ISODate = () => {
    return (new Date()).toISOString();
