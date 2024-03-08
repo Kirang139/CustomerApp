eb5f6c0ac1a49d95d4ebbdb54fd895b238ce5bde
Kiran200113 <kiran.gadani@hitechsmartsolutions.com>
2024-03-08 18:15:15 +0530

    test

--- src/app/api/services/Constants.ts
+++ src/app/api/services/Constants.ts
@@ -1,6 +1,6 @@
 // export const API_ENDPOINT = 'https://dashboard.revords.com/testapi/api/';
 // export const DownloadAPK_ENDPOINT = 'https://dashboard.revords.com/testapi/wwwroot/'
-
+//just for check
 export const API_ENDPOINT = 'http://ho.hitechprojects.co.in:8101/api/';
 export const DownloadAPK_ENDPOINT = 'http://ho.hitechprojects.co.in:8101/wwwroot/'
 
